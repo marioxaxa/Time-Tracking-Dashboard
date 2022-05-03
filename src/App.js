9730bfcbfc2511d1b444be12e8ffaf460cf8f987
mariusxaxae <melonston@gmail.com>
2022-05-03 03:06:39 -0300

    Primeiro dia

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+
+const Box = () => {
+  return (
+    <div>
+    <div className='Box-top'></div>
+    <div className='Box-bottom'>
+      <div className='Box-type'>
+        <p>Type</p>
+        <div>
+          <svg width="21" height="5" xmlns="http://www.w3.org/2000/svg">
+            <path d="M2.5 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Zm8 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Zm8 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Z" fill="#BBC0FF" fill-rule="evenodd"/>
+          </svg>
+        </div>
+      </div>
+      <div className='Box-time'>
+        <p>Xhrs</p>
+      </div>
+      <div className='Box-past'>
+        <p>Ultima Semana - Xhrs</p>
+      </div>
+    </div>
+  </div>
+  )
+}
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='Grid'>
+        <div>
+          <div className='Box-top' id='Profile-top'></div>
+          <div className='Box-bottom' id='Profile-bottom'></div>
+        </div>
+        <Box />
+        <Box />
+        <Box />
+        <Box />
+        <Box />
+        <Box />
+      </div>
     </div>
   );
 }
